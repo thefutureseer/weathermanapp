a9d7ef41d3972d0b72cea6a7fce37d83442fb7ad
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-10-22 10:59:03 -0700

    begin list container map to props render

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '8edc8745f9f9307f7a8e2bc2d1e4b4a3';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -9,7 +9,7 @@ export function fetchWeather(city) {
  const url = `${ROOT_URL}&q=${city},us`;
  const request = axios.get(url);
 
- console.log('Request:', request);
+// console.log('Request:', request);
 
  return {
   type: FETCH_WEATHER,
